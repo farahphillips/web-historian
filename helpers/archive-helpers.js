c388a3afbc669729262bf72c80c2f3e0bd8a65ee
fmaredia <farhana.maredia@gmail.com>
2015-07-01 19:18:41 -0500

    Download url Functional.

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,14 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-var http = require('../node_modules/http-request')
-
-/*
- * You will need to reuse the same paths many times over in the course of this sprint.
- * Consider using the `paths` object below to store frequently used file paths. This way,
- * if you move any files, you'll only need to change your code in one place! Feel free to
- * customize it in any way you wish.
- */
+var http = require('../node_modules/http-request');
+var request = require('../node_modules/request');
 
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
@@ -16,15 +10,11 @@ exports.paths = {
   list: path.join(__dirname, '../archives/sites.txt')
 };
 
-// Used for stubbing paths for tests, do not modify
 exports.initialize = function(pathsObj){
   _.each(pathsObj, function(path, type) {
     exports.paths[type] = path;
   });
 };
-
-// The following function names are provided to you to suggest how you might
-// modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback){
   fs.readFile(exports.paths.list, function(err, data){
@@ -57,22 +47,10 @@ exports.isUrlArchived = function(url, callback){
   })
 };
 
-// helper function for exports.downloadUrls
-exports.downloadFile = function(file_url) {
-  var file = fs.createWriteStream(exports.paths.archivedSites + '/' + file_url);
-
-  http.get(file_url, function(res) {
-    res.on('data', function(data) {
-      file.write(data);
-    }).on('end', function() {
-      file.end();
-    });
-  });
-};
-
 exports.downloadUrls = function(urlArray){
-  _.each(urlArray, function(url){
-    exports.downloadFile(url);
-    exports.addUrlToList(url);
-  })
+  for (var i = 0; i < urlArray.length; i++) {
+    var uri = "http://" + urlArray[i];
+    request(uri).pipe(fs.createWriteStream(exports.paths.archivedSites + '/' + urlArray[i]))
+    exports.addUrlToList(urlArray[i]);
+  }
 };
--- test/test.js
+++ test/test.js
@@ -142,13 +142,12 @@ describe("archive helpers", function(){
     it("should download all pending urls in the list", function (done) {
       var urlArray = ["www.example.com", "www.google.com"];
       archive.downloadUrls(urlArray);
-      debugger;
 
       // Ugly hack to wait for all downloads to finish.
       setTimeout(function () {
         expect(fs.readdirSync(archive.paths.archivedSites)).to.deep.equal(urlArray);
         done();
-      }, 25);
+      }, 2500);
     });
   });
 });
